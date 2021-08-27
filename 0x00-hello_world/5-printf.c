94497774448d1672568cfbb2a686a79331c5654a
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-08-27 13:00:55 -0700

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - printing the string in print function
  *
  * Description: using the main fuction
- * this program prints "with proper grammar, but the outcome is a piece of art,"
+ * this program prints with proper grammar, but the outcome is a piece of art,
  * Return: 0
  */
 int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,");
+{ printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
