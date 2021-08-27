81fbba5a77b35170786db139373f625c69fb9ae0
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-08-27 12:36:01 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- * main-print the string in the put function
+ * main - print the string in the put function
  *
  * Description: using the main function
  * this program prints "programming is like building a mutilingual puzzle
- * return:0
+ * Return: 0
  */
 int main(void)
 {
