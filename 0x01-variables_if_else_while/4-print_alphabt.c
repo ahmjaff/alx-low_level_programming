9c4e51a8aeb3629e774ee906698129692982dfd1
ahmjaff <ahmjaff@gmail.com>
2021-08-30 07:44:54 -0700

    printing lettrs except eq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main -a program that print a-z in lowercase and uppercase
+*
+* Description: using the main function
+* to print alphabet in lowercase and uppercase
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
+}
