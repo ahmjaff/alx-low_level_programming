741a4e6ecfba54a3ac70a04c6d4ebef50689cb92
ahmjaff <ahmjaff@gmail.com>
2021-08-30 08:16:44 -0700

    numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main -a program that print a-z in lowercase and uppercase
+*
+* Description: using the main function
+* to print alphabet in lowercase and uppercase
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i\n", i);
+}
+return (0);	       
+}
