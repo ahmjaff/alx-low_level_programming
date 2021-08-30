69abce31e1af4b926c7b4d5975e50a33f046bec6
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-08-30 09:16:59 -0700

    prints all possible combinations of three digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,64 +5,25 @@
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
-
-	while (c <= 99)
-	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-
-		if (f_d < l_d)
-		{
-			putchar(f_d);
-			putchar(l_d);
-
-			if (c != 89)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		c++;
-	}
-putchar('\n');
-return (0);
-}
-[4:44 AM, 8/30/2021] Usman alx: 11
-#include <stdio.h>
-/**
- * main - program that prints all possible different combinations of 3 digits.
- * Return: 0
- */
-int main(void)
+int c = 0;
+int f_d;
+int l_d;
+while (c <= 99)
 {
-	int c = 0;
-	int f_d;
-	int m_d;
-	int l_d;
-
-	while (c <= 999)
-	{
-		f_d = (c / 100 + '0');
-		m_d = (c / 10 % 10 + '0');
-		l_d = (c % 10 + '0');
-
-		if ((f_d < m_d) && (m_d < l_d))
-		{
-			putchar(f_d);
-			putchar(m_d);
-			putchar(l_d);
-
-			if (c != 789)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		c++;
-	}
+f_d = (c / 10 + '0');
+l_d = (c % 10 + '0');
+if (f_d < l_d)
+{
+putchar(f_d);
+putchar(l_d);
+if (c != 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
 putchar('\n');
 return (0);
 }
