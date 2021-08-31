b92840dbf1fd858946aefba6a4c27f5821a80eba
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-08-31 13:37:01 -0700

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,13 @@
 /**
  * main - practicing putchar
  * Description: using the main function
- * this program prints "holberton"
+ * this program prints "_putchar"
  * Return: 0
  */
 #include "main.h"
 int main(void)
 {
-char c[10] = "Holberton";
+char c[10] = "_putchar";
 int i;
 for (i = 0; i < 10; i++)
 {_putchar(c[i]);
