053fa264db01a57a1a13a1b435084e1d3ce6023f
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-10 12:46:54 -0700

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (b =< a && c =< a)
+if (b < a && c < a)
 {
 largest = a;
 }
-else if (a =< b && c =< b)
+else if (a < b && c < b)
 {
 largest = b;
 }
