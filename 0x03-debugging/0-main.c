2dc771414dae2db3bc92fa9dc3728d31cb7afbcc
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-10 11:29:15 -0700

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,6 +3,7 @@
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
+* @i: int value
 */
 
 int main(void)
@@ -10,5 +11,6 @@ int main(void)
 int i;
 i = 0;
 positive_or_negative(i);
+  printf("%d is zero", i);
 return (0);
 }
