2e5f559e0a6a1127c8578cc352ea6143db43d785
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-11 09:53:09 -0700

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
-* print_numbers - the function prnt 0-9
+* print_most_numbers - the function prnt 0-9
+* except 2 and 3
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
