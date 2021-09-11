b6d580c5a26daad35c8e41fc3346b7fa3c7ab02d
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-11 09:15:24 -0700

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - printing integers from 0-9
+*
+* return 0
+*/
+
+void print_numbers(void);
+int i;
+{
+for (i = 0; i <= 9; i++)
+{
+putchar(i)
+i++;
+}
+return (0);
+}
