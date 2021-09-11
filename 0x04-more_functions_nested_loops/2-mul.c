824d2d61814274fb7cbbbe4e64a3540fdae90568
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-11 06:11:36 -0700

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul(int a, int b) - function to check for lowercase character
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	a * b;
+		return (0);
+}
