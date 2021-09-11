8fde238065b160ca349426fafce297ae415a4aed
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-11 05:37:52 -0700

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+/**
+ * _isupper - function to check for lowercase character
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
