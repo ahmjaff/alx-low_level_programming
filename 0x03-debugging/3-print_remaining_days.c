963decfefee2056711fb6487c4dd0fc897fe9c6e
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-10 17:46:55 -0700

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month >= 12 && day >= 32)
+if (month == 13 && day == 32)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
