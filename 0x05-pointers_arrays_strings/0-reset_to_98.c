64dfe12843c023dcb755e571001e95ff9ee038dd
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-09-21 09:02:39 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,6 +6,7 @@
  * Description: using the reset_to_98(int *n) this program used pointers to
  * assign new value
  * @n: the pointer
+ * @reset_to_98: this is function
  * return: void
  */
 void reset_to_98(int *n)
