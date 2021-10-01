e6213dee1e452e34f8afd1b6987bc4e18bd97ac8
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-10-01 03:04:51 -0700

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@
 
 #include <stddef.h>
 
-int _putchar(char c);
+int _putchar(char);
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
