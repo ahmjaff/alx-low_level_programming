38ed385f580c347d6890f6e31a4032f395cba56d
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-10-05 12:40:21 -0700

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 /**
  * File: main.h
- * Auth: Amos Mwongela 
+ * Auth: Ahmad Jafar
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x06-pointers_arrays_strings directory.
  */
