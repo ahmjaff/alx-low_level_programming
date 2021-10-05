e4b8f25152f4b1d10b5ade28ddb62a593d4d3584
AHMAD JAFAR MUHAMMAD <ahmjaff@gmail.com>
2021-10-05 11:09:40 -0700

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /* more headers goes there */
 /**
-* main -a program that print a-z in lowercase 
+* main-a program that print a-z in lowercase 
 *
 * Description: using the main function
 * to print alphabet in lowercase
 * Return: 0
-*/int main()
+*/
+int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 putchar('\n');
-return 0;
+return (0);
 }
